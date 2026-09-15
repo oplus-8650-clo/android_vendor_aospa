5df94116170363a1838a05c1e165de0df481cea1
anky894 <anky894@proton.me>
2026-09-15 15:47:54 +0530

    bootanimation: Switch to pixel bootanimation
    aospa one is outdated now.
    
    Imported from https://github.com/PixelOS-AOSP/android_vendor_custom
    
    Change-Id: I8a879aabb7b909163cad0744be748b12e252349d

--- bootanimation/bootanimation.mk
+++ bootanimation/bootanimation.mk
@@ -10,8 +10,6 @@ else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
      PRODUCT_COPY_FILES += vendor/aospa/bootanimation/1080/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
      PRODUCT_COPY_FILES += vendor/aospa/bootanimation/1440/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),2160)
-     PRODUCT_COPY_FILES += vendor/aospa/bootanimation/2160/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
     ifeq ($(TARGET_BOOT_ANIMATION_RES),)
         $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
